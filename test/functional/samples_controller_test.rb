c7c4f5da21a543be0d8dfa57248a3b845c6807d9
Ryan Wallace <rywall@gmail.com>
2010-02-03 22:05:37 -0500

    App Cleanup.

--- test/functional/samples_controller_test.rb
+++ test/functional/samples_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class MaizeRecordsControllerTest < ActionController::TestCase
+class SamplesControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
