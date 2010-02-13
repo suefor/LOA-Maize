5dc620a4d1a150c37a576d88acaebf7e452d97e2
Ryan Wallace <rywall@gmail.com>
2010-02-13 15:54:43 -0500

    Earth now mirrors map.

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -13,29 +13,49 @@ function initMaps() {
         center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById('selector_map'), myOptions); 
+
+    selector_map = new google.maps.Map(document.getElementById('selector_map'), myOptions);
 }
 
 function initCB(instance) {
-    ge = instance;
-    ge.getWindow().setVisibility(true);
+    display_earth = instance;
+    display_earth.getWindow().setVisibility(true);
    
-    ge.getNavigationControl().setVisibility(ge.VISIBILITY_AUTO);
+    display_earth.getNavigationControl().setVisibility(display_earth.VISIBILITY_AUTO);
 
     // add some layers
-    ge.getLayerRoot().enableLayerById(ge.LAYER_BORDERS, true);
-    ge.getLayerRoot().enableLayerById(ge.LAYER_ROADS, true);
+    display_earth.getLayerRoot().enableLayerById(display_earth.LAYER_BORDERS, true);
+    display_earth.getLayerRoot().enableLayerById(display_earth.LAYER_ROADS, true);
 
-    // fetch a KML file and show it
-    var link = ge.createLink('');
-    link.setHref('http://sketchup.google.com/3dwarehouse/download?mid=28b27372e2016cca82bddec656c63017&rtyp=k2');
-
-    var networkLink = ge.createNetworkLink('');
-    networkLink.setLink(link);
-    networkLink.setFlyToView(true);
-
-    ge.getFeatures().appendChild(networkLink);
+    addObservers();
+    fetchKML();
 }
 
 function failureCB(errorCode) {
+}
+
+function addObservers() {
+    google.maps.event.addListener(selector_map, 'bounds_changed', function(event) {
+        // Get the current view
+        var lookAt = display_earth.getView().copyAsLookAt(display_earth.ALTITUDE_RELATIVE_TO_GROUND);
+
+        // Set new latitude and longitude values
+        lookAt.setLatitude(selector_map.getCenter().lat());
+        lookAt.setLongitude(selector_map.getCenter().lng());
+
+        // Update the view in Google Earth
+        display_earth.getView().setAbstractView(lookAt);
+    });
+}
+
+function fetchKML() {
+    // fetch a KML file and show it
+    var link = display_earth.createLink('');
+    link.setHref('http://sketchup.google.com/3dwarehouse/download?mid=28b27372e2016cca82bddec656c63017&rtyp=k2');
+
+    var networkLink = display_earth.createNetworkLink('');
+    networkLink.setLink(link);
+    networkLink.setFlyToView(true);
+
+    display_earth.getFeatures().appendChild(networkLink);
 }
