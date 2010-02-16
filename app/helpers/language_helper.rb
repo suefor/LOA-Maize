cbca8da7d2d84df013c42364bfb1ded9866de06b
Ryan Wallace <rywall@gmail.com>
2010-02-15 20:51:12 -0500

    Oops wrong spelling.

--- app/helpers/language_helper.rb
+++ app/helpers/language_helper.rb
@@ -1,7 +1,7 @@
 module LanguageHelper
   def change_language_link
     if I18n.locale == 'en' 
-      link_to 'Espagnol', "#{APP_CONFIG[:spanish_website_url]}#{request.env['REQUEST_URI']}", :style => 'float:right; margin-right: 5px'
+      link_to 'Español', "#{APP_CONFIG[:spanish_website_url]}#{request.env['REQUEST_URI']}", :style => 'float:right; margin-right: 5px'
     else
       link_to 'English', "#{APP_CONFIG[:english_website_url]}#{request.env['REQUEST_URI']}", :style => 'float:right; margin-right: 5px'
     end
