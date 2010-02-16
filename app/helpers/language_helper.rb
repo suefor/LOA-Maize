7090cc6ca46b8ca92f6d113498d6bae8a4781776
Ryan Wallace <rywall@gmail.com>
2010-02-15 22:00:12 -0500

    More subdomain work.

--- app/helpers/language_helper.rb
+++ app/helpers/language_helper.rb
@@ -1,9 +1,9 @@
 module LanguageHelper
   def change_language_link
     if I18n.locale == 'es'
-      link_to 'English', {:subdomain => 'en'}, :style => 'float:right; margin-right: 5px'
+      link_to 'English', url_for(params.merge(:subdomain => 'en')), :style => 'float:right; margin-right: 5px'
     else
-      link_to 'Español', {:subdomain => 'es'}, :style => 'float:right; margin-right: 5px'
+      link_to 'Español', url_for(params.merge(:subdomain => 'es')), :style => 'float:right; margin-right: 5px'
     end
   end
 end
