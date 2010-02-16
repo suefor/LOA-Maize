22ee9f551889f607277079fca735287b1e80290c
Ryan Wallace <rywall@gmail.com>
2010-02-15 20:53:18 -0500

    Rearrange the language link if statement so english is the default.

--- app/helpers/language_helper.rb
+++ app/helpers/language_helper.rb
@@ -1,9 +1,9 @@
 module LanguageHelper
   def change_language_link
-    if I18n.locale == 'en' 
-      link_to 'Español', "#{APP_CONFIG[:spanish_website_url]}#{request.env['REQUEST_URI']}", :style => 'float:right; margin-right: 5px'
-    else
+    if I18n.locale == 'es'
       link_to 'English', "#{APP_CONFIG[:english_website_url]}#{request.env['REQUEST_URI']}", :style => 'float:right; margin-right: 5px'
+    else
+      link_to 'Español', "#{APP_CONFIG[:spanish_website_url]}#{request.env['REQUEST_URI']}", :style => 'float:right; margin-right: 5px'
     end
   end
 end
